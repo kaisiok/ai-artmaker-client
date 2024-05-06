1d8c722aef47511de59aaa7e1b18d3a8dc50b41f
min <min@minui-MacBookPro.local>
2024-05-06 19:00:38 +0900

    tailwind-css 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import "./App.css";
 import Counter from "./components/Counter";
+import OutlineTypesExample from "./components/button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">hello world!</h1>
       <Counter />
+      <OutlineTypesExample />
     </div>
   );
 }
