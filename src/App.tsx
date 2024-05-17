c5941d5a90ff812a29423b0f364519ae84880a3d
min <min@minui-MacBookPro.local>
2024-05-17 17:25:09 +0900

    슬라이드 애니메이션 효과 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 import Counter from "./components/Counter";
 import OutlineTypesExample from "./components/button";
 import MyNavbar from "./components/navbars/Navbar";
 import MyFooter from "./components/footers/Myfooter";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./animations.css";
 
 import Home from "./components/bodys/Home";
 import Signup from "./components/bodys/Signup";
@@ -14,17 +16,25 @@ import Mypage from "./components/bodys/Mypage";
 import MyPhotos from "./components/bodys/Myphotos";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="Ap bg-gray-lv1">
       <MyNavbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tagselect-style" element={<Tagselect_style />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/myphotos" element={<MyPhotos />} />
       </Routes>
+      <SwitchTransition>
+        <CSSTransition key={location.pathname} classNames="slide" timeout={300}>
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/tagselect-style" element={<Tagselect_style />} />
+          </Routes>
+        </CSSTransition>
+      </SwitchTransition>
       <MyFooter />
     </div>
   );
