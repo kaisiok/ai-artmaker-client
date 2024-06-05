43ab430a9c7dea3999773394a16c367a6f9d23f2
min <min@minui-MacBookPro.local>
2024-06-05 14:49:10 +0900

    text img 생성 팁 추가, api 요청 바디 수정

--- src/components/bodys/Texttoimg.tsx
+++ src/components/bodys/Texttoimg.tsx
@@ -1,5 +1,11 @@
 import MyModal from "../MyModal";
-import { Form, FloatingLabel, Button, Spinner } from "react-bootstrap";
+import {
+  Form,
+  FloatingLabel,
+  Button,
+  Spinner,
+  Accordion,
+} from "react-bootstrap";
 import api from "../Api";
 
 import { useState, useEffect } from "react";
@@ -11,7 +17,7 @@ import { modalActions } from "../../store/modal";
 import { imgActions } from "../../store/img";
 
 function Texttoimg(): React.ReactElement {
-  const [imgStyle, setImgStyle] = useState("black and white illustration");
+  const [imgStyle, setImgStyle] = useState("001");
   const [imgText, setImgText] = useState("");
   const [textColor, setTextColor] = useState("text-gray-500");
   const [isLoading, setIsLoading] = useState(false);
@@ -28,10 +34,10 @@ function Texttoimg(): React.ReactElement {
   }, [imgText]);
 
   const styleList = [
-    { value: "black and white illustration", label: "흑백" },
-    { value: "oil painting", label: "유화" },
-    { value: "realistic", label: "실사" },
-    { value: "bishoujo", label: "레트로" },
+    { value: "001", label: "목탄 그림" },
+    { value: "002", label: "인상파 유화" },
+    { value: "003", label: "코믹 아트" },
+    { value: "004", label: "잉크 아트" },
   ];
 
   function handleClick(e: React.MouseEvent<HTMLInputElement, MouseEvent>) {
@@ -67,7 +73,7 @@ function Texttoimg(): React.ReactElement {
         console.log(err);
       }
     } finally {
-      setImgStyle("black and white illustration");
+      setImgStyle("001");
       setImgText("");
       setIsLoading(false);
     }
@@ -76,7 +82,7 @@ function Texttoimg(): React.ReactElement {
   return (
     <div className="mt-14 h-[calc(100svh-56px)]">
       <div className="bg-gray-lv2 h-full flex justify-center">
-        <div className="w-96 h-full bg-white pt-16 px-10 lg:px-20 lg:w-1/2 border-x-2 border-solid border-gray-lv3">
+        <div className="w-96 h-full bg-white pt-16 px-10 lg:px-20 lg:w-1/2 border-x-2 border-solid border-gray-lv3 overflow-auto ">
           <FloatingLabel
             controlId="floatingTextarea2"
             label="그림을 글로 묘사해 주세요(영어 권장)"
@@ -160,6 +166,57 @@ function Texttoimg(): React.ReactElement {
               </Button>
             )}
           </div>
+          <Accordion
+            defaultActiveKey="0"
+            flush
+            className="my-10 border-1 border-solid border-gray-lv3 rounded-lg overflow-hidden"
+          >
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>원하는 이미지를 얻기 위한 팁</Accordion.Header>
+              <Accordion.Body style={{ fontSize: "0.8rem", lineHeight: "1.5" }}>
+                <p>
+                  기본적으로 한글로 입력된 문장은 <strong>DeepL 번역기</strong>
+                  를 거쳐 영어로 번역됩니다.
+                </p>
+                <p>
+                  영어가 능숙하시다면, 영어로 문장을 입력하시는게 더 정확한
+                  결과를 얻을 수 있습니다.
+                </p>
+                <p>
+                  키워드를 나열해서 이미지를 생성하는 방법도 있습니다. 예를
+                  들어:
+                </p>
+                <ul>
+                  <li>
+                    <strong>one guy,</strong>
+                  </li>
+                  <li>
+                    <strong>working,</strong>
+                  </li>
+                  <li>
+                    <strong>sitting,</strong>
+                  </li>
+                  <li>
+                    <strong>desk,</strong>
+                  </li>
+                  <li>
+                    <strong>office,</strong>
+                  </li>
+                  <li>
+                    <strong>&#123;tired&#125;</strong>
+                  </li>
+                </ul>
+                <p>
+                  위와 같이 입력하면 사무실에서 일하는 피곤한 남자 그림을 얻을
+                  수 있습니다.(실제 입력에선 줄바꿈을 하면 안됩니다.)
+                </p>
+                <p>
+                  <strong>&#123; &#125;</strong> 는 키워드를 강조하기 위해
+                  사용됩니다.
+                </p>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </div>
       </div>
       <MyModal />
